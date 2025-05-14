eac4a6cd5376e9451de5c492d913dfd0d1f7130f
TTBirD <thutaminlu30@gmail.com>
2025-05-14 11:41:08 +0630

    First Commit

---
+++ src/main/java/com/db_test/Main.java
@@ -0,0 +1,23 @@
+package com.db_test;
+
+/**
+    This is the Testing Main Class
+    Created By: ...
+    Created Date: ...
+ */
+
+public class Main {
+    /**
+
+     */
+
+    public static void main(String[] args) {
+        // This is Output.
+        System.out.printf("Hello and welcome!");
+
+        // ......
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
