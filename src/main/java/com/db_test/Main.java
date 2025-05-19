0f28c5f3c251d7a6bea40c95c6d979843eed6d4b
TTBirD <thutaminlu30@gmail.com>
2025-05-19 15:13:11 +0630

    MySql

--- src/main/java/com/db_test/Main.java
+++ src/main/java/com/db_test/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
     public static void main(String[] args) {
         // This is Output.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
 
         // ......
         for (int i = 1; i <= 5; i++) {
